4d5854cd6d7716decbccadce45c346c5d9a99f5f
Ovenoboyo <ovenoboyo@gmail.com>
2022-08-13 12:45:14 +0530

    Add dummy class for unsupported os
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ module.exports = { ButtonEnum, PlaybackStateEnum }
 
 if (process.platform === "win32") {
   module.exports['MediaController'] = require('./win').MediaController
+} else if (process.platform === "linux") {
+  module.exports['MediaController'] = require('./linux').MediaController
+} else {
+  module.exports['MediaController'] = require('./dummy').MediaController
 }
 
-if (process.platform === "linux") {
-  module.exports['MediaController'] = require('./linux').MediaController
-}
+
