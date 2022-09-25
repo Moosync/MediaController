eaae44ecab8a862c06a2edf416392dc24c3c49ba
Ovenoboyo <ovenoboyo@gmail.com>
2022-09-25 21:34:48 +0530

    Fix enum types
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,24 +1,24 @@
-export const ButtonEnum: {
-  Play: 0
-  Pause: 1
-  Stop: 2
-  Record: 3
-  FastForward: 4
-  Rewind: 5
-  Next: 6
-  Previous: 7
-  ChannelUp: 8
-  ChannelDown: 9
-  Shuffle: 10
-  Repeat: 11
+export const enum ButtonEnum {
+  Play= 0,
+  Pause= 1,
+  Stop= 2,
+  Record= 3,
+  FastForward= 4,
+  Rewind= 5,
+  Next= 6,
+  Previous= 7,
+  ChannelUp= 8,
+  ChannelDown= 9,
+  Shuffle= 10,
+  Repeat= 11,
 }
 
-export const PlaybackStateEnum: {
-  Closed: 0
-  Changing: 1
-  Stopped: 2
-  Playing: 3
-  Paused: 4
+export const enum PlaybackStateEnum {
+  Closed = 0,
+  Changing= 1,
+  Stopped= 2,
+  Playing= 3,
+  Paused= 4
 }
 
 export type PlayerDetails = {
